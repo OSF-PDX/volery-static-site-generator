23c5617e043d6684a9b50440341fc7c118d30f7f
Abby Castillo <abby.machines@gmail.com>
2026-03-25 11:29:12 -0700

    clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import voleryLogo from './assets/volery-logo-sketch.png'
 import './App.css'
-
-
 
 function App() {
   const [error, setError] = useState(null);
 
-  // placeholder function
-const uploadCSV = () => {
-  setError("No upload function exists yet.");
-}
+  const uploadCSV = () => {
+    setError("No upload function exists yet.");
+  }
 
   return (
     <>
