b86c8687ac35296c62af40d8893236b5e6ac17a4
Abby Castillo <abby.machines@gmail.com>
2026-03-26 14:01:15 -0700

    wrap elements in form element

--- src/App.jsx
+++ src/App.jsx
@@ -19,26 +19,28 @@ function App() {
       </header>
       <main id="main">
         <h1>Volery</h1>
-        <p>
-          Conference name: <input />
-        </p>
-        <p>
-          <button
-            className="csv-upload"
-            onClick={uploadCSV}
-            aria-describedby='csv-error'
-          >
-            Upload CSV
-          </button>
-        </p>
-        <p id="csv-error" aria-live="polite" style={{color: 'red'}}>
-          {error && `Error: ${error}`}
-        </p>
-        <p>
-          <button disabled={true}>
-            Download site
-          </button>
-        </p>
+        <form>
+          <p>
+            Conference name: <input />
+          </p>
+          <p>
+            <button
+              className="csv-upload"
+              onClick={uploadCSV}
+              aria-describedby='csv-error'
+            >
+              Upload CSV
+            </button>
+          </p>
+          <p id="csv-error" aria-live="polite" style={{color: 'red'}}>
+            {error && `Error: ${error}`}
+          </p>
+          <p>
+            <button disabled={true}>
+              Download site
+            </button>
+          </p>
+        </form>
       </main>
     </>
   )
