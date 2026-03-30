a9093c08c08333f8f9b262dc4bb9fd6d2b64a2e8
skylos-computer <126717281+skylos-computer@users.noreply.github.com>
2026-03-30 10:05:21 -0700

    add functioning download code

--- src/proof_of_function.jsx
+++ src/proof_of_function.jsx
@@ -1,9 +1,10 @@
 import JSZip from "jszip";
 import nunjucks from "nunjucks";
+import streamSaver from "streamsaver"
 
 
 function render_from_template(){
-    nunjucks.configure('/dist/templates', { autoescape: true });
+    nunjucks.configure('/templates', { autoescape: true });
     return(nunjucks.render("test.njk", {username: "Alice"}));
 }
 
@@ -17,3 +18,47 @@ function RenderedTemplate(){
     )
 }
 
+function zip_with_folders(){
+    const zip = new JSZip;
+    zip.file("index.html", "hello, world!");
+    zip.file("index.html", "<h1>hello, world!</h1>"); //can overwrite files
+    const folder = zip.folder("foo_dir");
+    folder.file("foo.txt", "I'm in a folder");
+    zip.file("foo_dir/bar.txt", "I'm in a folder too");
+    zip.file("bad_file", "ew, I shouldn't be here");
+    zip.remove("bad_file");
+    return(zip);
+}
+
+async function save_zip(zip) {
+    // FILESYSTEM API NOT UNIVERSALLY SUPPORTED
+    // this is our entry point into the user's file system
+    // const saveHandle = window.showSaveFilePicker();
+    // const writableStream = await saveHandle.createWritable();
+
+    // NODESTREAM NOT SUPPORTED IN BROWSER
+    // zip.generateNodeStream({type:'uint8array'})
+    // .pipeTo(writableStream)
+    // .on("finish", ()=>{writableStream.close();});
+
+    const writableStream = streamSaver.createWriteStream('my-Volery.zip');
+    const writer =  writableStream.getWriter();
+    zip.generateInternalStream({type:'uint8array'})
+    .on('data', (data, metadata)=>{writer.write(data)})
+    .on('end', ()=>{writer.close()})
+    .resume(); //we registered the callbacks, now we start the stream
+}
+
+function DownloadButton(){
+    const zip = zip_with_folders();
+
+    return(
+        <>
+        <button onClick={()=>{save_zip(zip)}}>
+            Download zip!
+        </button>
+        </>
+    );
+}
+
+export {DownloadButton, RenderedTemplate}
