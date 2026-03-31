5bf0fab56db91b621e4cfb15fa08cd085261dc04
violet <126717281+skylos-computer@users.noreply.github.com>
2026-03-31 09:13:24 -0700

    put main.jsx back to normal

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// import GenerateStaticSiteButton from './template_test.jsx'
-import {RenderedTemplate, DownloadButton} from './proof_of_function.jsx'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>template test</h1>
-    <RenderedTemplate />
-    <DownloadButton />
+   <App />
   </StrictMode>,
 )
