4bb54760d664169761a6d426d9c5b34abac3ebbc
Gregory Propf <gpropf@gmail.com>
2026-04-25 10:43:55 -0700

    Set base URL for production environment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/volery-static-site-generator/' : '/',
   plugins: [react(), basicSsl()],
   build: {
     minify: false
