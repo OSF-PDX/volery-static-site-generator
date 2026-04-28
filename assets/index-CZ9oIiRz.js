fe177aa2ef4402a688b298593f06c259253870d8
gpropf <gpropf@users.noreply.github.com>
2026-04-28 15:47:39 +0000

    deploy: f11fd811e7a036a6b18974a61c4e755df8447ef2

--- assets/index-CZ9oIiRz.js
+++ assets/index-CZ9oIiRz.js
@@ -13397,7 +13397,6 @@ var require_react_jsx_runtime_production = /* @__PURE__ */ __commonJSMin(((expor
 var import_jsx_runtime = (/* @__PURE__ */ __commonJSMin(((exports, module) => {
 	module.exports = require_react_jsx_runtime_production();
 })))();
-console.log("Testing deployment after deleting github-worflow branch.");
 function App() {
 	const [error, setError] = (0, import_react.useState)(null);
 	const [csvText, setCSVText] = (0, import_react.useState)(null);
