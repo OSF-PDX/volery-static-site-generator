a704f25d2c5e5c2c6cd57f02a4c44b9897eda5d9
Greg Propf <gpropf@gmail.com>
2026-05-27 21:48:47 -0700

    Commas within single or double quotes are now escaped from the processing by the split function. Existing comma escaping is still supported as well.

--- src/services/csv-to-json.js
+++ src/services/csv-to-json.js
@@ -1,8 +1,44 @@
 const splitEscapedComma = (line) => {
-  return line
+  return escapeQuotedCommas(line)
     .split(/(?<!\\),/)
     .map((value) => value.replace(/\\,/g, ",").trim());
 };
+
+const escapeQuotedCommas = (line) => {
+  let output = "";
+  // This is a simple state machine to escape commas that are inside quoted strings.
+  // It handles both double and single quotes, and also allows for commas to be escaped with a backslash.
+  let MODE = "NORMAL";
+  for (let i = 0; i < line.length; i++) {
+    let c = line[i];
+    switch (MODE) {
+      case "NORMAL":
+        if (c === '"') {
+          MODE = "DBL_QUOTE";          
+        } else if (c === "'") {
+          MODE = "SGL_QUOTE";
+        }
+        break;
+      case "DBL_QUOTE":
+        if (c === '"') {
+          MODE = "NORMAL";
+        } else if (c === ",") {
+          c = "\\,";
+        }
+        break;
+      case "SGL_QUOTE":
+        if (c === "'") {
+          MODE = "NORMAL";
+        } else if (c === ",") {
+          c = "\\,";
+        }
+        break;
+    }
+    output += c;
+  }
+  return output;
+};
+
 
 export const csvToJSON = (filename, contents) => {
   // Parse CSV contents and convert to JSON
